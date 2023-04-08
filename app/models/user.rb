73e40634b4f35f7a0919f48a8f86e8f4dc4b44cc
Jesal Rathod <rathodjesal7@gmail.com>
2023-04-08 20:52:26 +0530

    Added Follow model associations with User model + Added User model associations test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+
+  # List of users followed by the user
+  has_many :followees, through: :followed_users, class_name: 'User'
+
+  # List of users who follow the user or List of followers
+  has_many :followers, through: :following_users, class_name: 'User'
+
+
   validates_presence_of :name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,24 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { User.new(name: 'Bob Marle') }
 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
+  describe "associations" do
+    it { should have_many(:following_users) }
+    it { should have_many(:followed_users) }
+    it { should have_many(:followers).through(:following_users) }
+    it { should have_many(:followees).through(:followed_users) }
   end
 
-  it "is not valid without a name" do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  context "with valid attributes" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
   end
+
+  context "with invalid attributes" do
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
 end
